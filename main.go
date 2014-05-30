5a983242894462ca6b1d783c7aaa28d5f4471e03
Peter Hellberg <peter@c7.se>
2014-05-30 17:08:14 +0200

    Added the logger-enabled flag
    Abandons the middleware.Logger if false

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"github.com/peterhellberg/wiki/db"
 	"github.com/peterhellberg/wiki/wiki"
 	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web/middleware"
 )
 
 var dbFile = flag.String("db", "/tmp/wiki.db", "Path to the BoltDB file")
+var loggerEnabled = flag.Bool("logger-enabled", true, "Enable/Disable logging to stdout")
 
 func main() {
 	flag.Parse()
@@ -25,6 +27,10 @@ func main() {
 	w, err := wiki.NewWiki(&db)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if *loggerEnabled != true {
+		goji.Abandon(middleware.Logger)
 	}
 
 	// Setup up the routes for the wiki
